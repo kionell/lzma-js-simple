27e29a7b1d2271989480b95aeb59b0477d661d05
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-08-03 19:14:42 +0000

    Export compress and decompress functions

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+type Mode = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+declare function compress(
+  str: string | ArrayLike<number> | Uint8Array | Buffer,
+  mode?: Mode,
+  on_finish?: (result: number[], error: any) => void,
+  on_progress?: (percent: number) => void
+): number[];
+
+declare function decompress(
+  byte_arr: ArrayLike<number>,
+  on_finish: (result: string | number[], error: any) => void,
+  on_progress: (percent: number) => void
+): string | number[];
+
+export { compress, decompress };
